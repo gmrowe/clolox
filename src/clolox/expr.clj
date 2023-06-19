0512d4cc095d14c40d6ac5fcc3480a4633b74193
Garrett Rowe <garrett.m.rowe@gmail.com>
2023-06-18 23:36:37 -0700

    Auto generated .clj file

---
+++ src/clolox/expr.clj
@@ -0,0 +1,32 @@
+(ns clolox.expr)
+
+(defn binary
+  [left-expr operator-token right-expr]
+  {
+   :left-expr left-expr
+   :operator-token operator-token
+   :right-expr right-expr
+   :tag :binary
+   })
+
+(defn grouping
+  [expr]
+  {
+   :expr expr
+   :tag :grouping
+   })
+
+(defn literal
+  [value]
+  {
+   :value value
+   :tag :literal
+   })
+
+(defn unary
+  [operator-token right-expr]
+  {
+   :operator-token operator-token
+   :right-expr right-expr
+   :tag :unary
+   })
