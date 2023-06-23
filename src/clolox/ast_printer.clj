2d251a58ec743be23950b0fadebedbb914aff83a
Garrett Rowe <garrett.m.rowe@gmail.com>
2023-06-22 23:46:15 -0700

    Refactor ast-printer to use multimethods

--- src/clolox/ast_printer.clj
+++ src/clolox/ast_printer.clj
@@ -2,34 +2,33 @@
   (:require [clojure.string :as str]
             [clolox.expr :as expr]))
 
-(declare parenthesize)
-
-(def printing-strategy
-  {:binary (fn [expr]
-             (parenthesize (get-in expr [::expr/operator-token :token/lexeme])
-                           (::expr/left-expr expr)
-                           (::expr/right-expr expr)))
-
-   :grouping (fn [expr]
-               (parenthesize "group" (::expr/expr expr)))
-
-   :literal (fn [expr]
-              (if-let [v (::expr/value expr)]
-                (str v)
-                "nil"))
-
-   :unary (fn [expr]
-            (parenthesize (get-in expr [::expr/operator-token :token/lexeme])
-                          (::expr/right-expr expr)))})
-
-(defn print-expr
-  [expr]
-  (let [f (get printing-strategy (::expr/tag expr))]
-    (f expr)))
+(defmulti print-expr ::expr/tag)
 
 (defn parenthesize
   [fname & exprs]
   (let [f (fn [tokens e] (conj tokens " " (print-expr e)))]
     (str/join (conj (reduce f ["(" fname] exprs) ")"))))
+
+(defmethod print-expr :binary
+  [expr]
+  (parenthesize (get-in expr [::expr/operator-token :token/lexeme])
+                (::expr/left-expr expr)
+                (::expr/right-expr expr)))
+
+(defmethod print-expr :grouping
+  [expr]
+  (parenthesize "group" (::expr/expr expr)))
+
+(defmethod print-expr :literal
+  [expr]
+  (if-let [v (::expr/value expr)]
+    (str v)
+    "nil"))
+
+(defmethod print-expr :unary
+  [expr]
+  (parenthesize (get-in expr [::expr/operator-token :token/lexeme])
+                (::expr/right-expr expr)))
+
 
 
