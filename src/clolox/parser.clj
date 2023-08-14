99b4188ad2904f92c1b1b943da8a45eecd4a5576
Garrett Rowe <garrett.m.rowe@gmail.com>
2023-08-13 22:39:37 -0700

    Implement expression parser

---
+++ src/clolox/parser.clj
@@ -0,0 +1,126 @@
+(ns clolox.parser
+  (:require [clolox.expr :as expr]
+            [clolox.logger :as logger]
+            [clolox.token :as t]))
+
+(defn parser
+  [tokens]
+  #:parser{:tokens tokens
+           :current 0})
+
+(defn nth-token
+  [psr n]
+  (nth (:parser/tokens psr) n))
+
+(defn peek-token
+  [psr]
+  (nth-token psr (:parser/current psr)))
+
+(defn at-end?
+  [psr]
+  (= (:token/type (peek-token psr)) ::t/eof))
+
+(defn advance
+  [psr]
+  (when-not (at-end? psr)
+    (update psr :parser/current inc)))
+
+(defn previous
+  [psr]
+  (nth-token psr (dec (:parser/current psr))))
+
+(defn check
+  [psr token-type]
+  (and (not (at-end? psr)) (= (:token/type (peek-token psr)) token-type)))
+
+(defn match
+  [psr & token-types]
+  (some
+   (fn [token-type] (= (:token/type (peek-token psr)) token-type))
+   token-types))
+
+(defn error
+  [token msg]
+  (logger/error token msg)
+  (ex-info
+   "ParseError"
+   {:token token
+    :msg msg}))
+
+(defn consume
+  [psr token-type msg]
+  (if (check psr token-type)
+    (advance psr)
+    (throw (error (peek-token psr) msg))))
+
+(declare expression)
+
+(defn primary
+  [psr]
+  (cond
+    (match psr ::t/false) [(expr/literal false) (advance psr)]
+    (match psr ::t/true) [(expr/literal true) (advance psr)]
+    (match psr ::t/nil) [(expr/literal nil) (advance psr)]
+
+    (match psr ::t/number ::t/string)
+    [(expr/literal (:token/literal (peek-token psr))) (advance psr)]
+
+    (match psr ::t/left-paren)
+    (let [[expr p] (expression (advance psr))]
+      [(expr/grouping expr)
+       (consume p ::t/right-paren "Expect ')' after expression.")])))
+
+(defn unary
+  [psr]
+  (if (match psr ::t/bang ::t/minus)
+    (let [[right p1] (unary (advance psr))]
+      [(expr/unary (peek-token psr) right) p1])
+    (primary psr)))
+
+(defn factor
+  [psr]
+  (let [[left p] (unary psr)]
+    (loop [expr left
+           p p]
+      (if (match p  ::t/slash ::t/star)
+        (let [operator (peek-token p)
+              [right p1] (unary (advance p))]
+          (recur (expr/binary expr operator right) p1))
+        [expr p]))))
+
+(defn term
+  [psr]
+  (let [[left p] (factor psr)]
+    (loop [expr left
+           p p]
+      (if (match p ::t/plus ::t/minus)
+        (let [operator (peek-token p)
+              [right p1] (factor (advance p))]
+          (recur (expr/binary expr operator right) p1))
+        [expr p]))))
+
+(defn comparison
+  [psr]
+  (let [[left p] (term psr)]
+    (loop [expr left
+           p p]
+      (if (match p ::t/greater ::t/greater-equal ::t/less ::t/less-equal)
+        (let [operator (peek-token p)
+              [right p1] (term (advance p))]
+          (recur (expr/binary expr operator right) p1))
+        [expr p]))))
+
+(defn equality
+  [psr]
+  (let [[left p] (comparison psr)]
+    (loop [expr left
+           p p]
+      (if (match p ::t/bang-equal ::t/equal-equal)
+        (let [operator (peek-token p)
+              [right p1] (comparison (advance p))]
+          (recur (expr/binary expr operator right) p1))
+        [expr p]))))
+
+(defn expression
+  [psr]
+  (equality psr))
---
+++ test/clolox/parser_test.clj
@@ -0,0 +1,51 @@
+(ns clolox.parser-test
+  (:require [clojure.test :refer :all]
+            [clolox.ast-printer :as ast-printer]
+            [clolox.parser :refer :all]
+            [clolox.scanner :as scanner]
+            [clolox.token :as token]))
+
+(defn current-lexeme-matches
+  [psr expected-lexeme]
+  (is (= (:token/lexeme (peek-token psr)) expected-lexeme)))
+
+(defn parsed-expr-matches
+  [expr-string expected]
+  (let [tokens (scanner/scan-tokens (scanner/scanner expr-string))
+        parser (parser tokens)
+        [expr _] (expression parser)]
+    (is (= (ast-printer/print-expr expr) expected))))
+
+(deftest parser-test
+  (testing "Primary matches"
+    (parsed-expr-matches "false" "false")
+    (parsed-expr-matches "true" "true")
+    (parsed-expr-matches "nil" "nil")
+    (parsed-expr-matches "420" "420.0")
+    (parsed-expr-matches "\"This is a string\"" "This is a string")
+    (parsed-expr-matches "(420)" "(group 420.0)"))
+
+  (testing "Unary matches"
+    (parsed-expr-matches "!true" "(! true)")
+    (parsed-expr-matches "-420" "(- 420.0)")
+    (parsed-expr-matches "!!false" "(! (! false))")
+    (parsed-expr-matches "-(-260)" "(- (group (- 260.0)))"))
+
+  (testing "Factor matches"
+    (parsed-expr-matches "2 * 3" "(* 2.0 3.0)")
+    (parsed-expr-matches "4 / -0.69"  "(/ 4.0 (- 0.69))")
+    (parsed-expr-matches "-(1.69 * 3)" "(- (group (* 1.69 3.0)))"))
+
+  (testing "Term matches"
+    (parsed-expr-matches "2 + 3" "(+ 2.0 3.0)")
+    (parsed-expr-matches "2 * 3 + 5 / 6" "(+ (* 2.0 3.0) (/ 5.0 6.0))")
+    (parsed-expr-matches
+     "(2 * 2) + (4 * 5)"
+     "(+ (group (* 2.0 2.0)) (group (* 4.0 5.0)))"))
+
+  (testing "Comparison matches"
+    (parsed-expr-matches "5 > 3 " "(> 5.0 3.0)")
+    (parsed-expr-matches "2 <= (-4 + 6)" "(<= 2.0 (group (+ (- 4.0) 6.0)))"))
+
+  (testing "Equality matches"
+    (parsed-expr-matches "3 == 3 != 4" "(!= (== 3.0 3.0) 4.0)")))
